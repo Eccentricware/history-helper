417174e94f215f94141f84a60a1316e4c08700b7
Joseph Peck <joe.r.peck@gmail.com>
2023-09-09 14:24:25 -0700

    Selected standings and sorting for partial and full leagues

--- src/database/queries/get-all-decks-query.ts
+++ src/database/queries/get-all-decks-query.ts
@@ -3,6 +3,37 @@ export const getAllDecksQuery = `
   FROM decks
   ORDER BY constructed DESC,
     points_per_tournament_16 DESC,
-    tournaments_16;
+    tournaments_16,
+    deck_name;
+`;
+
+export const getStandings16Query = `
+  SELECT deck_name,
+    color_id,
+    commander,
+    points_per_tournament_16,
+    points_16,
+    tournaments_16
+  FROM decks
+  ORDER BY constructed DESC,
+    points_per_tournament_16 DESC,
+    tournaments_16,
+    color_sort_order,
+    color_id_number;
+`;
+
+export const getStandings64Query = `
+  SELECT deck_name,
+    color_id,
+    commander,
+    points_per_tournament_64,
+    points_64,
+    tournaments_64
+  FROM decks
+  ORDER BY constructed DESC,
+    points_per_tournament_64 DESC,
+    tournaments_64,
+    color_sort_order,
+    color_id_number;
 `;
 
