b57564e474e1096ec3ba3072720a8a20e75a1d80
Joseph Peck <joe.r.peck@gmail.com>
2023-10-25 19:54:12 -0700

    Restructured tables and added a getFighterFavorQuery

--- src/server/routes/ultimate-router.ts
+++ src/server/routes/ultimate-router.ts
@@ -4,6 +4,16 @@ import { UltimateService } from '../services/ultimate-service';
 export const ultimateRouter = express.Router();
 const ultimateService = new UltimateService();
 
+ultimateRouter.get('/get-roster', (request, response) => { 
+  ultimateService.getRoster()
+    .then((roster: any) => {
+      response.send({ roster: roster });
+    })
+    .catch((error: Error) => {
+      response.send({ error: `Error getting roster: ${error.message}`});
+    })
+};
+
 ultimateRouter.post('/select-fighters', (request, response) => {
   const { playerId, poolSize } = request.body;
 
